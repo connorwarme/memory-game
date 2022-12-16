c4f2266a75a13f8aa2c51c0760cee3676cf4d6d3
connorwarme <connor.warme@gmail.com>
2022-12-16 16:50:19 -0700

    Add styling for in-between sizes (small tablet?)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,19 @@ import "../styles/Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-        <div className="footerContent">
-          <div className="linkContainer">
-            <a href="https://github.com/connorwarme">
-              <img src={Github} alt="GitHub Profile"></img>
-            </a>
-            <a href="https://www.linkedin.com/in/connor-warme-103a09167">
-              <img src={Linked} alt="LinkedIn Profile" target="_blank"></img>
-            </a>
-          </div>
-          <div className="textContainer">
-            <div className="footerText">Peregrinning Productions</div>
-          </div>
-        </div>
+            <div className="footerContent">
+            <div className="linkContainer">
+                <a href="https://github.com/connorwarme">
+                <img src={Github} alt="GitHub Profile"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/connor-warme-103a09167">
+                <img src={Linked} alt="LinkedIn Profile" target="_blank"></img>
+                </a>
+            </div>
+            <div className="textContainer">
+                <div className="footerText">Peregrinning Productions</div>
+            </div>
+            </div>
       </div>
     )
 }
