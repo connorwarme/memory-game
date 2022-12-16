dc25e73c0f76aa3bc62456c431e0cfa485b536cd
connorwarme <connor.warme@gmail.com>
2022-12-16 15:58:58 -0700

    Add Footer and content, continue styling

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Github from "../imgs/github.svg";
+import Linked from "../imgs/linkedin.svg";
+import "../styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+        <div className="footerContent">
+          <div className="linkContainer">
+            <a href="https://github.com/connorwarme">
+              <img src={Github} alt="GitHub Profile"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/connor-warme-103a09167">
+              <img src={Linked} alt="LinkedIn Profile" target="_blank"></img>
+            </a>
+          </div>
+          <div className="textContainer">
+            <div className="footerText">Peregrinning Productions</div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Footer;
